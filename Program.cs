edb037d06816c7532d0c1622df6c32da61ee0efa
eliaa04 <eliatodorova10@gmail.com>
2024-03-22 15:47:07 +0200

    Implemented the main program flow

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace CourseProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.WriteLine("Въведете команда:");
+                string command = Console.ReadLine();
+
+                string commandName = string.Empty;
+
+                foreach (var symbol in command)
+                {
+                    if (Char.IsWhiteSpace(symbol))
+                    {
+                        break;
+                    }
+
+                    commandName += symbol;
+                }
+
+                switch (commandName.ToUpperInvariant())
+                {
+                    case "DEFINE":
+                        Console.WriteLine("bla bla define");
+                        break;
+                    case "SOLVE":
+                        Console.WriteLine("bla bla solve");
+                        break;
+                    case "ALL":
+                        Console.WriteLine("bla bla all");
+                        break;
+                    default:
+                        Console.WriteLine("Командата не е разпозната. Въведете валидна команда!");
+                        break;
+                }
+            }
+        }
+    }
+}
