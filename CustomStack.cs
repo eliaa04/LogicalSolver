d5bfcc1142b097b40a527cc16dcc65ebb0bf08f1
eliaa04 <eliatodorova10@gmail.com>
2024-05-23 15:05:08 +0300

    CustomStack implementation.

---
+++ CustomStack.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalSolver
+{
+    public class CustomStack
+    {
+        private List<TreeNode> _nodes = new List<TreeNode>();
+
+        public void Push(TreeNode node) => _nodes.Add(node);
+
+        public TreeNode Peek() => _nodes[_nodes.Count - 1];
+
+        public TreeNode Pop()
+        {
+            var node = Peek();
+            _nodes.RemoveAt(_nodes.Count - 1);
+            return node;
+        }
+    }
+}
--- Tree.cs
+++ Tree.cs
@@ -1,14 +1,8 @@
-﻿ using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogicalSolver
+﻿namespace LogicalSolver
 {
     public class Tree
     {
-        private Stack<TreeNode> _stack = new Stack<TreeNode>();
+        private CustomStack _stack = new CustomStack();
 
         public TreeNode BuildTree(string funcBody)
         {
